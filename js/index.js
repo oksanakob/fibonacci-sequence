73ab83dd82711d4f4e55ccbb11beefb6901084a8
Oksana <oksanakob21@gmail.com>
2018-11-23 15:16:45 +0000

    some changes

--- js/index.js
+++ js/index.js
@@ -1,10 +1,25 @@
-const n = parseInt(prompt "Enter your number:");
+const n = parseInt(prompt ("Enter your number:"));
 
+//first method
 
-function fib(n){
-  let arr = [0, 1];
-  for (let i = 2; i < n + 1; i++){
-    arr.push(arr[i - 2] + arr[i -1])
+function fib(n) {
+  var a = 1, b = 1;
+  for (var i = 3; i <= n; i++) {
+    var c = a + b;
+    a = b;
+    b = c;
   }
- return arr[n]
+  return b;
 }
+
+
+/* second method
+
+function fib(n) {
+  if (n < 2){
+    return n;
+  }
+  return fib(n - 1) + fib(n - 2);
+}*/
+
+alert(fib(b));
